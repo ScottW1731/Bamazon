cc07394f46ac15eed43718133ffc5955672fc12f
Scott Wagner <af17317596@gmail.com>
2019-04-08 23:53:30 -0500

    function to select products use inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,6 +12,7 @@ var connection = mysql.createConnection({
 connection.connect(function(error){
     if (error) throw error ;
     // here the function to show all the products goes
+    console.log("SUCCESS!!!")
     displayAll();
 })
 
@@ -28,13 +29,53 @@ function displayAll(){
         }
         console.log("\n");
         // function will allow user to select a product from the database
-
+        customerSelection();
     })
 }
 
 // function to select products use inquirer 
 // function makes promise
 // how to solve a promise
+
+
+function customerSelection(){
+    // why the brackets?
+    // calling inquirer from earlier prompting the user
+    inquirer.prompt([
+        {
+            type: "list",
+            feedback:"Choose the id of your desired product.",
+            selection: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+            name:"selection"
+        },
+        {
+            type: "input",
+            feedback:"How many do you want to buy?",
+            name:"quantity"
+        },
+        {
+            type: "confirm",
+            feedback:"Are you sure?",
+            name:"purchase",
+            default: true
+        }
+    ])
+    // why the  ".then"  what is it?
+    .then(function(response){
+        var selection = response.selection;
+        var quantity = parseInt(response.quantity);
+        var confirm = response.purchase;
+
+        // validates purchase, the sell
+        if (confirm) {
+            validatingPurchase(selection, quantity);
+
+            // prompt for user if otherwise to send through 
+        } else{
+            customerSelection();
+        }
+    });
+}
 // create function to process a purchase
 // update info on products
 // get new input
