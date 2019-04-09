7f999e12546ad4177534aba99f48f0419a885282
Scott Wagner <af17317596@gmail.com>
2019-04-09 13:34:13 -0500

    almost done added in validate purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,37 +20,43 @@ function displayAll(){
     connection.query("SELECT * FROM products", function(error, response){
         if (error) throw error;
         for(var i= 0; i < response.length; i++){
-            console.log('-------------------');
-            console.log('ID: ' + response[i].id);
-            console.log('Product Name: ' + response[i].product_name);
-            console.log('Department Name: ' + response[i].department_name);
-            console.log('Price: ' + response[i].price);
-            console.log('Stock Quantity: ' + response[i].stock_quantity);
+            // console.log('-------------------');
+            // console.log('ID: ' + response[i].id);
+            // console.log('Product Name: ' + response[i].product_name);
+            // console.log('Department Name: ' + response[i].department_name);
+            // console.log('Price: ' + response[i].price);
+            // console.log('Stock Quantity: ' + response[i].stock_quantity);
+            // console.log('-------------------');
+            console.log(' ID: ' + response[i].id +' || Product Name: ' + response[i].product_name +' ||  Department Name: ' + response[i].department_name
+             +' ||  Price: ' + response[i].price +' ||  Stock capacity: ' + response[i].stock_capacity+ "  \n");
         }
-        console.log("\n");
+        // console.log("\n");
         // function will allow user to select a product from the database
         customerSelection();
     })
 }
 
 // function to select products use inquirer 
+
+// what is a promise?
 // function makes promise
 // how to solve a promise
 
 
 function customerSelection(){
-    // why the brackets?
+    
     // calling inquirer from earlier prompting the user
+    // user choses product from list
     inquirer.prompt([
         {
             type: "list",
             feedback:"Choose the id of your desired product.",
-            selection: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-            name:"selection"
+            choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+            name:"choices"
         },
         {
             type: "input",
-            feedback:"How many do you want to buy?",
+            feedback:"How many do you want to buy? Exit with X.",
             name:"quantity"
         },
         {
@@ -62,22 +68,62 @@ function customerSelection(){
     ])
     // why the  ".then"  what is it?
     .then(function(response){
-        var selection = response.selection;
-        var quantity = parseInt(response.quantity);
+        var choices = response.choices;
+        var quantity  = parseInt(response.quantity);
         var confirm = response.purchase;
 
         // validates purchase, the sell
         if (confirm) {
-            validatingPurchase(selection, quantity);
+            validatingPurchase(choices, quantity);
 
-            // prompt for user if otherwise to send through 
+            // prompt for user if otherwise to send through again
         } else{
             customerSelection();
         }
     });
 }
+
+
 // create function to process a purchase
-// update info on products
+function validatingPurchase(choices, quantity) {
+    // what is the "WHERE id=?" ?
+    connection.query("SELECT * FROM products WHERE id=?", [choices], function(error, response){
+        if(error) throw error;
+        var price;
+        var currentCapacity;
+        // update info on products
+
+        if(quantity <= parseInt(response[0].stock_capacity)){
+            currentCapacity = parseInt(response[0].stock_capacity) - quantity;
+
+            // call update to the products after purchase
+            productUpdate(currentCapacity, choices);
+
+
+            // display price of total items bought
+            price = parseInt(response[0].price);
+            cost = quantity * price;
+            console.log("\n Your total is:" + cost.toFixed(2)+ "\n")
+
+            // see if they will buy more
+            buyMore();
+        } else {
+            // if user selects an amount that is impossible then prompt user so
+            // and send back to select correctly
+            console.log("I am sorry, you an invalid selection!");
+            customerSelection();
+        }
+    });
+}
+
+
+// update original capacity minus purchased amount
+function updateCapacity (quantity, choices){
+    var query = connection.query(
+        "UPDATE products SET stock_capacity=? WHERE id=?",
+    [quantity, choices],
+    )
+}
+
 // get new input
 // option to change input function
-
--- schema.sql
+++ schema.sql
@@ -8,37 +8,37 @@ id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
 price DECIMAL(10,2) NOT NULL,
-stock_quantity INT NOT NULL,
+stock_capacity INT NOT NULL,
 PRIMARY KEY (id)
 );
 
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("LizardmenModels", 'RareplasticToys', 20.30, 100);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("StarWarsTrilogyOriginal", 'DVDs', 450.00, 50);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("ArmyBuilderforPC", 'software', 120.50, 50);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("ChaosModels", 'RareplasticToys', 29.80, 500);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("HighelfModels", 'RareplasticToys', 505.60, 80);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("Starcraft2Legacyofthevoid", 'PCGames', 105.52, 30);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("ForHonor", 'PCGames', 48.28, 500);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("HoratioHorblowerseries", 'DVDs', 5.99, 300);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("EmireOfSigmarModels", 'RareplasticToys', 509.99, 80);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
+INSERT INTO products ( product_name, department_name, price, stock_capacity)
 VALUES ("SkavenModels", 'RareplasticToys', 399.99, 70);
